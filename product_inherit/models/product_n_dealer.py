c0b5950dedcb3ebad520025410b7b35945ec80bd
Golam Mostafa <sujon1mostafa@gmail.com>
2021-04-20 23:36:14 +0600

    taskupdate1

--- product_inherit/models/product_n_dealer.py
+++ product_inherit/models/product_n_dealer.py
@@ -4,14 +4,14 @@ from odoo.exceptions import UserError
 
 class ProductInfo(models.Model):
     _name = 'product.wiki'
-    _description = 'Product Info'
+    _description = 'Product Brand'
     _rec_name = 'pname'
 
-    pname = fields.Char(string='Product Name')
+    pname = fields.Char(string='Product Brand')
 
 class DealerInfo(models.Model):
     _name = 'dealer.wiki'
-    _description = 'Dealer Info'
+    _description = 'Dealer Name'
     _rec_name = 'dname'
 
     dname = fields.Char(string='Dealer Name')
--- purchase_extend/models/purchase_new.py
+++ purchase_extend/models/purchase_new.py
@@ -5,12 +5,16 @@ class PurchaseNew(models.Model):
     _inherit = 'purchase.order'
 
     dealer = fields.Many2one('dealer.wiki', string='Dealer Name')
-
+    
 class PurchaseLineNew(models.Model):
     _inherit = 'purchase.order.line'
 
-    brand_id = fields.Many2one('product.wiki', string='Brand', readonly=True)
+    brand_id = fields.Many2one('product.wiki', string='Brand')
     @api.onchange('product_id')
     def onchange_test(self):
         if self.product_id:
             self.brand_id = self.product_id.product_brand.id
+
+    # def write(self, values):
+    #     values['brand_id'] = self.product_id.product_brand.id
+    #     return super(PurchaseLineNew, self).write(values)
