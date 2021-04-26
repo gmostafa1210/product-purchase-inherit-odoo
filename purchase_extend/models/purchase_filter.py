bec6f9dae943a19e1f0f51a44cf12386a44ec920
Golam Mostafa <sujon1mostafa@gmail.com>
2021-04-26 22:40:00 +0600

    Date and filter added

--- purchase_extend/models/purchase_filter.py
+++ purchase_extend/models/purchase_filter.py
@@ -9,28 +9,33 @@ class PurchaseFilter(models.TransientModel):
     date_from = fields.Date(string="From")
     date_to = fields.Date(string="To")
 
-    # is_true = fields.Boolean(string="Want to Filter")
+    is_true = fields.Boolean(string="Want to Filter")
 
     def get_purchase_filter(self):
         q_list = []
         d_list = []
+        my_list = []
 
-        # if self.is_true:
-        #     product_ids = self.env['purchase.order.line'].search([('qty_received','>',0),('qty_invoiced','<', qty_received)])
-        
-        product_ids = self.env['purchase.order.line'].search([])
-        for i in product_ids:
-            if i.qty_received > 0 and i.qty_invoiced < i.qty_received:
-                q_list.append(i.order_id.id)
-        
-        product_date_ids = self.env['purchase.order'].search([('date_approve','>=',self.date_from),('date_approve','<=',self.date_to)])
-     
-        for j in product_date_ids:
-            d_list.append(j.id)
+        if self.is_true:
+            product_ids = self.env['purchase.order.line'].search([])
+            for i in product_ids:
+                if i.qty_received > 0 and i.qty_invoiced < i.qty_received:
+                    q_list.append(i.order_id.id)
             
-        q_list_as_set = set(q_list)
-        intersection = q_list_as_set.intersection(d_list)
-        my_list = list(intersection)
+            product_date_ids = self.env['purchase.order'].search([('date_approve','>=',self.date_from),('date_approve','<=',self.date_to)])
+        
+            for j in product_date_ids:
+                d_list.append(j.id)
+                
+            q_list_as_set = set(q_list)
+            intersection = q_list_as_set.intersection(d_list)
+            my_list = list(intersection)
+
+        else:
+            product_date_ids = self.env['purchase.order'].search([('date_approve','>=',self.date_from),('date_approve','<=',self.date_to)])
+        
+            for j in product_date_ids:
+                my_list.append(j.id)
                 
         return {
             'name': 'Filtered Orders',
