23e01a96cd13f705688b86a2e321e1bd45f425d3
Golam Mostafa <sujon1mostafa@gmail.com>
2021-04-26 23:12:22 +0600

    filter date option added

--- purchase_extend/models/purchase_filter.py
+++ purchase_extend/models/purchase_filter.py
@@ -9,14 +9,15 @@ class PurchaseFilter(models.TransientModel):
     date_from = fields.Date(string="From")
     date_to = fields.Date(string="To")
 
-    is_true = fields.Boolean(string="Want to Filter")
+    add_filter = fields.Boolean(string="Add Filter")
+    add_date = fields.Boolean(string="Add Date")
 
     def get_purchase_filter(self):
         q_list = []
         d_list = []
         my_list = []
 
-        if self.is_true:
+        if self.add_filter and self.add_date:
             product_ids = self.env['purchase.order.line'].search([])
             for i in product_ids:
                 if i.qty_received > 0 and i.qty_invoiced < i.qty_received:
@@ -31,11 +32,23 @@ class PurchaseFilter(models.TransientModel):
             intersection = q_list_as_set.intersection(d_list)
             my_list = list(intersection)
 
-        else:
+
+        elif self.add_date:
             product_date_ids = self.env['purchase.order'].search([('date_approve','>=',self.date_from),('date_approve','<=',self.date_to)])
         
             for j in product_date_ids:
                 my_list.append(j.id)
+
+
+        elif self.add_filter:
+            product_ids = self.env['purchase.order.line'].search([])
+            for i in product_ids:
+                if i.qty_received > 0 and i.qty_invoiced < i.qty_received:
+                    my_list.append(i.order_id.id)
+
+        else:
+            my_list = []
+
                 
         return {
             'name': 'Filtered Orders',
