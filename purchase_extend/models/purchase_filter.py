1172415f2333c9944f3e34a7c45c4cfb0dde21bd
Golam Mostafa <sujon1mostafa@gmail.com>
2021-04-26 12:33:52 +0600

    Filter Added

--- purchase_extend/models/purchase_filter.py
+++ purchase_extend/models/purchase_filter.py
@@ -14,7 +14,6 @@ class PurchaseFilter(models.TransientModel):
     def get_purchase_filter(self):
         q_list = []
         d_list = []
-        my_list = []
 
         # if self.is_true:
         #     product_ids = self.env['purchase.order.line'].search([('qty_received','>',0),('qty_invoiced','<', qty_received)])
@@ -23,21 +22,22 @@ class PurchaseFilter(models.TransientModel):
         for i in product_ids:
             if i.qty_received > 0 and i.qty_invoiced < i.qty_received:
                 q_list.append(i.order_id.id)
-
         
-        # product__date_ids = self.env['purchase.order'].search([('date_approve','>',self.date_from),('date_approve','>',self.date_to)])
-
-        # for j in product__date_ids:
-        #     if j.date_approve > self.date_from and j.date_approve < self.date_to:
-        #         d_list.append(j.order_id.id)
+        product_date_ids = self.env['purchase.order'].search([('date_approve','>=',self.date_from),('date_approve','<=',self.date_to)])
+     
+        for j in product_date_ids:
+            d_list.append(j.id)
+            
+        q_list_as_set = set(q_list)
+        intersection = q_list_as_set.intersection(d_list)
+        my_list = list(intersection)
                 
-
         return {
             'name': 'Filtered Orders',
             'type': 'ir.actions.act_window',
             'res_model': 'purchase.order',
             'view_mode': 'tree,form',
-            'domain': [('id','in',q_list)]
+            'domain': [('id','in',my_list)]
         }
         
          
